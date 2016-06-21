b78da9b7af55f9a599483649a4a1324e7fe22037
C0deZ Lee <c0dezlee@gmail.com>
2016-06-21 01:39:39 -0400

    Update run.sh

--- projects/llvm-deps/test/run.sh
+++ projects/llvm-deps/test/run.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 # linking example
 
-CPPFLAGS=
-LLVMLIBS=
-LDFLAGS=
+# CPPFLAGS=
+# LLVMLIBS=
+# LDFLAGS=
+
+
 # if your instrumentation code calls into LLVM libraries, then comment out the above and use these instead:
-#CPPFLAGS=`llvm-config --cppflags`
-#LLVMLIBS=`llvm-config --libs`
-#LDFLAGS=`llvm-config --ldflags`
+CPPFLAGS=`llvm-config --cppflags`
+LLVMLIBS=`llvm-config --libs`
+LDFLAGS=`llvm-config --ldflags`
 
 ## compile the instrumentation module to bitcode
 ## clang $CPPFLAGS -O0 -emit-llvm -c sample.cpp -o sample.bc
 clang -O0 -emit-llvm -o test.bc -c test.c
 
 ## opt -load *.so -infoflow < $BENCHMARKS/welcome/welcome.bc -o welcome.bc
-~/llvm-dsa/Debug+Asserts/bin/opt  -load ~/llvm-dsa/projects/poolalloc/Debug+Asserts/lib/LLVMDataStructure.so -load /home/ubuntu/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/Constraints.so  -load /home/ubuntu/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/sourcesinkanalysis.so -load /home/ubuntu/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/pointstointerface.so -load /home/ubuntu/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/Deps.so  -infoflow  -debug < ~/test/test.bc > /dev/null
+~/llvm-dsa/Debug+Asserts/bin/opt  -load ~/llvm-dsa/projects/poolalloc/Debug+Asserts/lib/LLVMDataStructure.so -load ~/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/Constraints.so  -load ~/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/sourcesinkanalysis.so -load ~/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/pointstointerface.so -load ~/llvm-dsa/projects/llvm-deps/Debug+Asserts/lib/Deps.so  -infoflow  -debug < ~/llvm-dsa/projects/llvm-deps/test/test.bc > /dev/null
 
 ## link instrumentation module
 #llvm-link welcome.bc sample.bc -o welcome.linked.bc
@@ -26,4 +28,3 @@ clang -O0 -emit-llvm -o test.bc -c test.c
 #g++ welcome.o $LLVMLIBS $LDFLAGS -o welcome
 
 #./welcome
-
